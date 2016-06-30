de01934bc18cd6621b7b16c4b9a9eb751fe035fa
Kevin Branch <kevin@branchnetconsulting.com>
2016-06-29 23:03:02 -0400

    Update auto_ossec.py

--- auto_ossec.py
+++ auto_ossec.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 #
-# auto_ossec.py - BNC fork - version 1.4
+# auto_ossec.py - BNC fork - version 1.5
 #
 # This is a fork by Kevin Branch (Branch Network Consulting) of BinaryDefense's auto_ossec.py.
 #
@@ -29,13 +29,14 @@ import os
 import subprocess
 import time
 import re
+import getopt 
 
 # try to import python-crypto
 try:
 	from Crypto.Cipher import AES
 
 except ImportError:
-	print ("[!] You need python-crypto in order for this module to work. If this is Ubuntu/Redhat - package name is python-crypto")
+	print ("[!] You need python-crypto in order for this module to work. If this is Ubuntu/Redhat install package python-crypto.")
 	sys.exit()
 
 # check platform specific installs
@@ -46,42 +47,88 @@ if platform.system() == "Linux":
 if platform.system() == "Windows":
 	installer = "Windows"
 
+if platform.system() == "Darwin":
+	installer = "Mac"
+
+# Check if OSSEC agent is installed.   If not, warn user and abort
+if not os.path.isfile("C:\\Program Files (x86)\\ossec-agent\\ossec-agent.exe") and not os.path.isfile("C:\\Program Files\\ossec-agent\\ossec-agent.exe") and not os.path.isfile("/var/ossec/bin/ossec-agentd"):
+	print ("[!] OSSEC agent is not installed on this system.  Install it before attempting to register it.")
+	sys.exit()
+
 if installer == "": 
-	print ("[!] Unable to determine operating system. Only supports Linux and Windows. Exiting..")
+	print ("[!] Unable to determine operating system. Only supports Linux, Windows, and Mac OS. Exiting..")
 	sys.exit()
 
-#
-# NEED TO DEFINE THIS AS THE OSSEC SERVER HOST THAT IS RUNNING SERVER.PY
-#
-try: 
-	host = sys.argv[1]
+def showhelp():
+        print ("""
+***************************************************************************************************************************
 
-except IndexError:
-	print ("""
-******************************************************
-Binary Defense Systems OSSEC Auto Enrollment, version 1.4
-(forked version by Branch Network Consulting)
+Auto-Enroll for OSSEC, version 1.5 by Kevin Branch (Branch Network Consulting)
+Forked from the original work of David Kennedy (Binary Defense Systems): Auto-Enroll for OSSEC, version 1.3
 
-In order for this to work, you need to point
-auto_ossec.exe to the OSSEC server that is
-listening. Note that default port is 9654
-but this can be changed in the source.  
+To enroll this OSSEC agent with an OSSEC server, run this program (auto_ossec.py or auto_ossec.exe)
+specifying at a minimum the IP or name of the OSSEC server that is running auto_server.py.
 
-You may also specify one or more config-profile names (csv, no spaces)
-as a second parameter to be referenced by agent.conf on the OSSEC server.
+auto_ossec.(py|exe) -s/--server OSSEC-SERVER [-h/--hostname HOSTNAME] [-n/--net NETWORK] [-p/--profile PROFILE(S)] [--help]
 
-Usage: auto_ossec.exe <server_ip> <optional_config_profile_name(s)>
-like: auto_ossec.exe 1.2.3.4 webserver,debian
+        OSSEC-SERVER - IP of OSSEC server running auto_server.py
+        HOSTNAME - optional name to self register as, if you want to override the local host name
+        NETWORK - network this agent will be limited to approaching the server from.  (0.0.0.0/0 means no restriction)
+        PROFILE(S) - one or more OSSEC config profiles this agent is to be associated with for custom configs from server
+                     (multiple profiles are allowed in csv format with no white space)
 
-*****************************************************
-		""")
+Usage example: auto_ossec.exe -s hids.xyz.com -n 192.168.150.0/24 -p finance,sensitive
+
+***************************************************************************************************************************
+                """)
+
+try:
+	options, remainder = getopt.getopt(sys.argv[1:], 's:h:n:p:', ['server=','hostname=','net=','profile=','help'])
+	server = ""
+	hostname = socket.gethostname()
+	network = "none"
+	profile = "none"
+
+	for opt, arg in options:
+	    if opt == '--help':
+	        showhelp()
+		sys.exit()
+	    elif remainder:
+	        print "\nInvalid extra parameter(s): ",remainder
+	        showhelp()
+		sys.exit()
+	    elif opt in ('-s', '--server'):
+		server = arg
+	    elif opt in ('-h', '--hostname'):
+	        hostname = arg
+	    elif opt in ('-n', '--net'):
+	        network = arg
+	    elif opt in ('-p', '--profile'):
+	        profile = arg
+
+except Exception as error:
+	print
+        print (error)
+	showhelp()
+	sys.exit()
+
+if server == "":
+        print "\nYou must at least specify OSSEC server name or IP (-s/--server).\n"
+        showhelp()
 	sys.exit()
 
 try: 
-	oprofile = sys.argv[2]
+	socket.inet_aton(server)
+except Exception as error:
+        print "\nInvalid IP address for server parameter."
+        showhelp()
+        sys.exit()
 
-except IndexError:
-	oprofile = "generic"
+pattern = re.compile("^(?:\d{1,3}\.){3}\d{1,3}(?:/\d\d?)?$")
+if network != "none" and not pattern.match(network):
+	print "\nThe specified network is not in valid IP CIDR format (like 192.168.200.0/24)"
+        showhelp()
+        sys.exit()
 
 def aescall(secret, data, format):
 
@@ -108,31 +155,30 @@ def aescall(secret, data, format):
                 aes = DecodeAES(cipher, data)
                 return str(aes)
 
-# this will grab the hostname and ip address and return it
-def grab_info():
-        try:
-                hostname = socket.gethostname()
-                return hostname #+ " " + ipaddr
-	
-        except Exception:
-                sys.exit()
 try:
         # Secret key - change this to something unique for your organization.  Keep the number of characters the same.
 	# Make sure to also change the secret key in auto_server.py on the OSSEC server to the same value.
+	# AES key must be 32 bytes long
 	secret = "ABABABABABABABABABABABABABABABAB"
+
         # port for daemon
         port = 9654 
         # general length size of socket
         size = 1024 
 
         print ("[*] auto_ossec - OSSEC agent mass deployment script")
-        print ("[*] Branch Network Consulting fork, version 1.4")
+        print ("[*] Branch Network Consulting fork, version 1.5")
 
-        # loop through in case server isnt reachable
+	print '[*] OSSEC Server     :', server
+	print '[*] Agent Hostname   :', hostname
+	print '[*] Agent Network    :', network
+	print '[*] Agent Profile(s) :', profile
+
+        # loop through in case server isn't reachable
         while 1:
                 try:
                         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-                        s.connect((host,port))
+                        s.connect((server,port))
                         break
 
                 except Exception: 
@@ -140,15 +186,22 @@ try:
                         time.sleep(10)
                         pass 
 
-        print ("[*] Connected to auto enrollment server at IP: " + host)
+        print ("[*] Connected to auto enrollment server at: " + server )
 
-        # grab host info needed for ossec
-        data = grab_info()
+        # grab local host name to register agent under, unless this has been overridden with -h/--hostname
+        if hostname == "":
+        	try:
+	                hostname = socket.gethostname()
+		
+	        except Exception:
+	                sys.exit()
 
         # encrypt the data
-        data = "BNCOSSEC" + data.rstrip()
+        data = "BNCOSSEC," + hostname.rstrip() + "," + network.rstrip()
+	unencrypted = data
+
         data = aescall(secret, data, "encrypt")
-        print ("[*] Pulled hostname and IP, encrypted data, and now sending to server.")
+        print "[*] Sending registration request to server: ",unencrypted
         s.send(data) 
 
 	# Handle incoming messages from the registration server until key is issued
@@ -182,7 +235,7 @@ try:
                 # import the key with the key presented from the server daemon
                 filewrite = file(path + "\\client.keys", "w")
 
-        if installer == "Linux":
+        if installer in "Linux|Mac":
                 if os.path.isfile(path + "/etc/client.keys"): os.remove("etc/client.keys")
                 filewrite = file(path + "/etc/client.keys", "w")
 
@@ -190,61 +243,50 @@ try:
         data = base64.b64decode(data)
         filewrite.write(data)
         filewrite.close()
-        #subprocess.Popen("echo y | manage_agents.exe -i %s" % data, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True).wait()
         print ("[*] Successfully imported the new pairing key.")
 
         print ("[*] Stopping the OSSEC service, just in case its running.")
-        # stop the service if it is
+        # stop the service if it is running
         if installer == "Windows":
                 subprocess.Popen('net stop "OSSEC HIDS"', stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True).wait()
 
-        if installer == "Linux":
-                if os.path.isfile("/etc/init.d/ossec"):
-                         subprocess.Popen("service ossec stop", stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True).wait()
-                if os.path.isfile("/etc/init.d/ossec-hids-agent"):
-                         subprocess.Popen("service ossec-hids-agent stop", stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True).wait()
+        if installer in "Linux|Mac":
+                subprocess.Popen("/var/ossec/bin/ossec-control stop", stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True).wait()
 
-        print ("[*] Creating a new ossec.conf to incorporate server host IP address and optional config-profile.")
-        # make sure we modify the ossec.conf
+        print ("[*] Creating a new ossec.conf to incorporate server address and optional config-profile.")
 
         if installer == "Windows":
                 filewrite = file(path + "\\ossec.conf", "w")
-                filewrite.write(" <ossec_config>\n")
-                filewrite.write("   <client>\n")
-                filewrite.write("      <server-ip>%s</server-ip>\n" % (host))
-                filewrite.write("      <config-profile>%s</config-profile>\n" % (oprofile))
-                filewrite.write("   </client>\n")
-                filewrite.write(" </ossec_config>\n")
-                filewrite.close()
-		ridslist = glob.glob("c:\\Program Files (x86)\\ossec-agent\\rids\\*")
+		ridslist = glob.glob(path + "\\rids\\*")
 
-        if installer == "Linux":
+        if installer in "Linux|Mac":
                 filewrite = file(path + "etc/ossec.conf", "w")
-                filewrite.write(" <ossec_config>\n")
-                filewrite.write("   <client>\n")
-                filewrite.write("    <server-ip>%s</server-ip>\n" % (host))
-                filewrite.write("      <config-profile>%s</config-profile>\n" % (oprofile))
-                filewrite.write("   </client>\n")
-                filewrite.write(" </ossec_config>\n")
-                filewrite.close()
 		ridslist = glob.glob("/var/ossec/etc/rids/*")
 
+        filewrite.write(" <ossec_config>\n")
+        filewrite.write("   <client>\n")
+        filewrite.write("    <server-ip>%s</server-ip>\n" % (server))
+        if profile != "none":
+             	filewrite.write("      <config-profile>%s</config-profile>\n" % (profile))
+        filewrite.write("   </client>\n")
+        filewrite.write(" </ossec_config>\n")
+        filewrite.close()
+
 	# If any rids files exist from a previous registration, delete them to avoid out of sync rids problems
-	for f in ridslist: os.remove(f)
+	for f in ridslist:
+		os.remove(f)
 
         # start the service
         if installer == "Windows":
                 subprocess.Popen('net start "OSSEC HIDS"', stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True).wait() 
-        if installer == "Linux":
-                if os.path.isfile("/etc/init.d/ossec"):
-                         subprocess.Popen("service ossec start", stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True).wait()
-                if os.path.isfile("/etc/init.d/ossec-hids-agent"):
-                         subprocess.Popen("service ossec-hids-agent start", stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True).wait()
+        if installer in "Linux|Mac":
+                subprocess.Popen("/var/ossec/bin/ossec-control start", stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True).wait()
 
         print ("[*] Finished. Started the OSSEC service. Auto Enrollment for OSSEC is now finished.")
+	s.close()
 
 except Exception as error:
         print ("[*] Something did not complete. Does this system have Internet access?")
 	tb = sys.exc_info()[2]
-	print tb.tb_lineno
+	print "Line: ",tb.tb_lineno
         print (error)
